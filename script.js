a007a0eae2386c1ab5f5e35db822106a7257848e
Daniel Gonzalez <danielcabilan@hotmail.com>
2023-04-22 14:25:04 -0600

    Agregando funcionalidad a boton desencriptar y borrarAT ademas de comentarios en el codigo

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Traemos todos los botones y las areas de texto al js
 var areaTexto = document.getElementById("areaTexto");
 var areaResultado = document.getElementById("areaResultado");
 var botonEncriptar = document.getElementById("botonEncriptar");
@@ -5,7 +6,9 @@ var botonDesencriptar = document.getElementById("botonDesencriptar");
 var botonCopiar = document.getElementById("botonCopiar");
 var botonBorrarAT = document.getElementById("botonBorrarAT");
 var botonBorrarR = document.getElementsByName("botonBorrarR");
+// -----------------------
 
+//Funciones Encriptar y Desencriptar
 function encriptar(textoUsuario){
     let textoEncriptado = textoUsuario.replace(/e/g,"enter").replace(/i/g,"imes").replace(/a/g,"ai").replace(/o/g,"ober").replace(/u/g,"ufat");
     return textoEncriptado; 
@@ -15,9 +18,22 @@ function desencriptar(textoEncriptado){
     let textoDesencriptado = textoEncriptado.replace(/enter/g,"e").replace(/imes/g,"i").replace(/ai/g,"a").replace(/ober/g,"o").replace(/ufat/g,"u");
     return textoDesencriptado;
 }
+//--------
 
-botonEncriptar.addEventListener("click", function(){
-    let textoUsuario = areaTexto.value;
+//Funcionalidades de los botones
+botonEncriptar.addEventListener("click", function(){ 
+    let textoUsuario = areaTexto.value; // Ponemos el texto escrito por el usuario en una variable
     let textoEncriptado = encriptar(textoUsuario);
-    areaResultado.value = textoEncriptado; 
+    areaResultado.value = textoEncriptado; //Enviamos directamente el texto encriptado al area de resultado 
+});
+
+botonDesencriptar.addEventListener("click", function(){
+    let textoEncriptado = areaTexto.value;
+    let textoDesencriptado = desencriptar(textoEncriptado);
+    areaResultado.value = textoDesencriptado; //Todos los resultados van hacia el area de resultados
+});
+
+botonBorrarAT.addEventListener("click", function(){
+    areaTexto.value = "";
+    areaTexto.focus(); //regresamos el focus al area de texto para seguir ecribiendo
 });
